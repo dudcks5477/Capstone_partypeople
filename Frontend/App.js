f88b6c15528105e8b21613bb7a410bae415553cd
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-01 23:38:09 +0900

    업로드

---
+++ Frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>지혁준 씨발</Text>
+      <Text></Text>
+      <Text></Text>
+    </View>
+  );
+};
+export default App;

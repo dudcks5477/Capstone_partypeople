a94f820e072beff9fc3cb86f3293d680244bbe97
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-02 00:24:42 +0900

    걍 버튼 만들어봄

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {TouchableOpacity,Text, View} from 'react-native';
 const App = () => {
   return (
     <View>
       <Text>app.tsx랑 app.js파일 두개여서 하나 지움</Text>
-      <Text></Text>
       <Text>또 술 먹니</Text>
+      <MyButton/>
+      <MyButton/>
     </View>
+    
+  );
+};
+const MyButton = () => {
+  return (
+      <TouchableOpacity
+          style={{ 
+          backgroundColor: '#3498db',
+          padding: 16,
+          margin: 10,
+          borderRadius: 8,
+          }}
+          onPress={() => alert('Click!!!')}
+      >
+          <Text style={{ color: 'white', fontSize: 24}}>d Button</Text>
+      </TouchableOpacity>
   );
 };
 export default App;

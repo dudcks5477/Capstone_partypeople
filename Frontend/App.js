cbeef4b60ebbebc6715fb9274c9d57bf89b511fc
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-02 00:15:34 +0900

    app.tsx랑 app.js파일 두개여서 하나 지움

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,8 +3,8 @@ import {Text, View} from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text>바보 영찬</Text>
-      <Text>바보 영찬</Text>
+      <Text>app.tsx랑 app.js파일 두개여서 하나 지움</Text>
+      <Text></Text>
       <Text>또 술 먹니</Text>
     </View>
   );

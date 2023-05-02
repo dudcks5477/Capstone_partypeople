dc2b88a6cf8faab8c6c332e657fe16027bf016ac
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-02 23:49:01 +0900

    Restart

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>1</Text>
+      <Text>2</Text>
+      <Text></Text>
+    </View>
+  );
+};
+export default App;

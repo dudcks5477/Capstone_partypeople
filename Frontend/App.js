b72fb5f596557a76eb785167f6dbaaaa579841a7
dudcks5477 <dudcks5477@naver.com>
2023-05-02 14:28:06 +0900

    First page

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import {TouchableOpacity, Text, View} from 'react-native';
+import {ScrollView, TextInput} from 'react-native';
 const App = () => {
   return (
-    <View>
+    <ScrollView>
       <Text>로그인</Text>
-      <Text>app.tsx랑 app.js파일 두개여서 하나 지움</Text>
-      <Text>또 술 먹니</Text>
-      <MyButton />
-      <MyButton />
-    </View>
-  );
-};
-
-const MyButton = () => {
-  return (
-    <TouchableOpacity
-      style={{
-        backgroundColor: '#3498db',
-        padding: 16,
-        margin: 10,
-        borderRadius: 8,
-      }}
-      onPress={() => alert('Click!!!')}>
-      <Text style={{color: 'white', fontSize: 24}}>d Button</Text>
-    </TouchableOpacity>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="ID"
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Password"
+      />
+    </ScrollView>
   );
 };
 export default App;

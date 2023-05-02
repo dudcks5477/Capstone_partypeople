7278afa4401935a3d45a085058c522b7aa4c4b5d
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-02 22:28:22 +0900

    seperate folder and make screen

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailScreen from './src/screens/DetailScreen';
 import LoginScreen from './src/LoginScreen';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return <LoginScreen />;
+  return (
+  <NavigationContainer>
+    <Stack.Navigator initial RouteName="Home"> 
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Detail" component={DetailScreen} /> 
+      <Stack.Screen name="Login" component={LoginScreen} /> 
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+  
 }
---
+++ Frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'; import {Button, View} from 'react-native';
+function HomeScreen({navigation}) {
+return (
+<View>
+    <Button title="Detail 열기" 
+    onPress={()=>navigation.navigate('Detail')}
+    />
+</View>
+);
+}
+export default HomeScreen;

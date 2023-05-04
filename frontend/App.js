8b3214ede49c1cbbdd13f8aa1b98bccc0b6eb234
dudcks5477 <dudcks5477@naver.com>
2023-05-04 22:09:50 +0900

    Listings is Card

--- frontend/App.js
+++ frontend/App.js
@@ -4,17 +4,19 @@ import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
 import UnderBarButton from './src/container/UnderBarButton';
 import HomeScreen from './src/screens/HomeScreen';
+import Listings from './src/components/Listings'
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="BottomTab" component={UnderBarButton} options={{ headerShown: false }} />
-        <Stack.Screen name="HomeSearchBar" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Listings/>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Login" component={LoginScreen} />
+    //     <Stack.Screen name="HomeSearchBar" component={HomeScreen} />
+    //     <Stack.Screen name="BottomTab" component={UnderBarButton} options={{ headerShown: false }} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 };
 

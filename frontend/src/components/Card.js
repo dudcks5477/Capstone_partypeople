26d69cc74fb3f45f1c77092a67317973bb8c2768
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-05 00:44:01 +0900

    home 화면 만듬 리스트없애고 백엔드랑 연결 필요, card 누르면 상세 정보 들어가는 screen 필요

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import {View, Image, Text, StyleSheet} from 'react-native';
+import {View, Image, Text, StyleSheet, SafeAreaView} from 'react-native';
 
-const Card = ({ image, title, description, price }) => {
-  return (
-    <View style={styles.card}>
-      <Image source={image} style={styles.image} />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
-      <Text style={styles.price}>{price}</Text>
+const Card = ({image, title, description, price}) => (
+  
+    <View style={styles.cardContainer}>
+      <View style={styles.card}>
+        <Image source={image} style={styles.image} />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <Text style={styles.price}>{price}</Text>
+      </View>
     </View>
-  );
-};
+  
+);
 
 const styles = StyleSheet.create({
+  
+  cardContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   card: {
     backgroundColor: '#fff',
-    borderRadius: 10,
+    borderRadius: 16,
     overflow: 'hidden',
     elevation: 5,
     margin: 10,
-    width: '48%',
-    aspectRatio: 1,
+    width: '90%',
+    aspectRatio: 1.5,
   },
   image: {
     width: '100%',

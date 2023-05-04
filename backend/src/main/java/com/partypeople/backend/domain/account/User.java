c7de3695eb92e1486e29fd3eaeb55015263bcf5d
지채영 <wlcodud7@naver.com>
2023-05-05 00:18:11 +0900

    # account

--- backend/src/main/java/com/partypeople/backend/domain/account/User.java
+++ backend/src/main/java/com/partypeople/backend/domain/account/User.java
@@ -1,9 +1,17 @@
 package com.partypeople.backend.domain.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "users")
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
+    @Id
     private String id;
 
     private String name;
@@ -12,38 +20,6 @@ public class User {
 
     private String password;
 
-    public  User(){}
-
-    public User(String name, String email, String password){
-        this.name=name;
-        this.email=email;
-        this.password=password;
-    }
-
-    public String getId(){
-        return id;
-    }
-    public void setId(String id){
-        this.id=id;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
     @Override
     public String toString(){
         return "User{"+

43b6b82077f8f56102dacf05ea0f2b9078242bbd
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-06 07:53:29 +0900

    생성하기 하면 일로가짐

---
+++ frontend/src/screens/AddScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AddScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>APP Screen</Text>
+    </View>
+  );
+};
+
+export default AddScreen;

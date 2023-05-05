6ab40b8affcee77b5798768b880c2b16b055122e
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-06 07:51:52 +0900

    지도 검색하면 이동하는 기능 추가 , 근데 핀 고정되서 검색하고나면 못움직임

--- frontend/src/screens/MapScreen.js
+++ frontend/src/screens/MapScreen.js
@@ -1,24 +1,35 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  Dimensions,
-  TouchableOpacity,
-  Text,
-  Image,
-} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, View, Dimensions,TouchableOpacity,Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+
 
 const MapScreen = () => {
-  const [position, setPosition] = useState(null);
+  const mapRef = useRef(null); // Ref를 추가합니다.
+  const navigation = useNavigation();
+  const [region, setRegion] = useState({
+    latitude: 37.5665,
+    longitude: 126.9780,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }); // 지도 초기 위치를 상태로 관리합니다.
 
-  const handleLongPress = ({nativeEvent}) => {
-    const {coordinate} = nativeEvent;
-    setPosition(coordinate);
-  };
+  const handlePlaceSelected = (data, details) => {
+    const { geometry } = details;
+    const { location } = geometry;
+    const { lat, lng } = location;
 
-  const handlePress = () => {
-    // Create and save location using position state
+    // 검색한 위치로 지도를 이동시킵니다.
+    mapRef.current.animateToRegion({
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: region.latitudeDelta,
+      longitudeDelta: region.longitudeDelta,
+    });
   };
 
   return (
@@ -26,19 +37,55 @@ const MapScreen = () => {
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        showsUserLocation={true}
-        onLongPress={handleLongPress}>
-        {position && <Marker coordinate={position} />}
+        ref={mapRef} // Ref를 추가합니다.
+        initialRegion={region} // 초기 위치를 상태에서 가져옵니다.
+        onRegionChangeComplete={setRegion} // 지도를 이동할 때마다 상태를 업데이트합니다.
+      >
+        <Marker coordinate={region} />
       </MapView>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-      <Image source={require('../assets/seoul.jpeg')} style={styles.image} />
-        <Text style={styles.text}>생성하기</Text>
-      </TouchableOpacity>
+
+      {/* GooglePlacesAutocomplete를 추가합니다. */}
+      <GooglePlacesAutocomplete
+              minLength={2}
+              placeholder="언제든 파티 위치 검색"
+              query={{
+                key: 'AIzaSyB_eawVNI3T9lGyQsePWA2GwE2uXyWxZI0',
+                language: "ko",
+                components: "country:kr",
+              }}
+              keyboardShouldPersistTaps={"handled"}
+              fetchDetails={true}
+              onPress={handlePlaceSelected}
+              onFail={(error) => console.log(error)}
+              onNotFound={() => console.log("no results")}
+              keepResultsAfterBlur={true}
+              enablePoweredByContainer={false}
+              styles={styles.search}
+            />
+            <TouchableOpacity
+    style={styles.addButton}
+    onPress={() => navigation.navigate('AddScreen')}
+  >
+    <Text style={styles.addButtonText}>+생성하기</Text>
+  </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  search: {
+    // container 감싸고 있는 컴포넌트
+      container: {
+        alignItems: 'center',
+        opacity: 0.8, // 투명도 설정
+        marginTop: 20,},
+    // input을 감싸는 컴포넌트
+      textInputContainer: {
+        
+        borderRadius : 20,
+        flexDirection: "row",
+        width: "90%", // 여기서 width를 수정합니다.
+  }},
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
@@ -47,28 +94,41 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  button: {
-    position: 'absolute',
-    bottom: 10,
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 1},
-    shadowOpacity: 0.5,
-    shadowRadius: 1,
-    elevation: 2,
+  textInputContainer: {
+    width: '100%',
+    backgroundColor: 'white',
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
   },
-  image: {
-    width: 24,
-    height: 24,
-    marginRight: 10,
-  },
-  text: {
+  textInput: {
+    height: 38,
+    color: '#5d5d5d',
     fontSize: 16,
-    fontWeight: 'bold',
+    borderRadius: 0,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+  },
+  listView: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    borderTopWidth: 0,
+  },
+  addButton: {
+    opacity : 0.8,
+    position: 'absolute',
+    bottom: 500,
+    right: 20,
+    width: 100,
+    height: 30,
+    backgroundColor: '#cccccc',
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButtonText: {
+    fontSize: 10,
+    color: 'white',
   },
 });
 

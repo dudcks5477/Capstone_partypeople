916ab7b416cae165fc674b32c63014d2b8415f30
dudcks5477 <dudcks5477@naver.com>
2023-05-06 23:47:09 +0900

    Add Wishilist

--- frontend/src/screens/WishlistScreen.js
+++ frontend/src/screens/WishlistScreen.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
 
 const WishlistScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Wishlist Screen</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Text style={styles.title}>Wishlists</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <Image source={require('../assets/party1.jpeg')} style={styles.cardImage} />
+        <Text style={styles.cardText}>찬's 파티 3월 18일 6시</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <Image source={require('../assets/party1.jpeg')} style={styles.cardImage} />
+        <Text style={styles.cardText}>찬's 파티 3월 18일 6시</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <Image source={require('../assets/party1.jpeg')} style={styles.cardImage} />
+        <Text style={styles.cardText}>찬's 파티 3월 18일 6시</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <Image source={require('../assets/party1.jpeg')} style={styles.cardImage} />
+        <Text style={styles.cardText}>찬's 파티 3월 18일 6시</Text>
+      </View>
+    </ScrollView>
+    
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#000',
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginTop: 30,
+  },
+  cardContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  cardImage: {
+    width: 362,
+    height: 154,
+    borderRadius: 8,
+    shadowColor: '#000000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+  },
+  cardText: {
+    position: 'absolute',
+    left: 16,
+    bottom: 16,
+    textAlign: 'left',
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
 
 export default WishlistScreen;

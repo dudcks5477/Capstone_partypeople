d24b11150801b6c8d48ae2f4880cafb4ce2588a6
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-06 23:23:55 +0900

    생성하기에 시간 날짜 파티이름 인원 기능추가

--- frontend/src/screens/AddScreen.js
+++ frontend/src/screens/AddScreen.js
@@ -1,10 +1,83 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Platform, Text, TextInput } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const AddScreen = () => {
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const [partyName, setPartyName] = useState('');
+  const [numOfPeople, setNumOfPeople] = useState('');
+  const [description, setDescription] = useState('');
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  const [mode, setMode] = useState('date');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>APP Screen</Text>
+    <View>
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+        <Text>선택한 날짜: {date.toLocaleDateString()}</Text>
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+        <Text>선택한 시간: {mode === 'time' && date.toLocaleTimeString()}{'\n'}</Text>
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+
+      <View>
+        <Text>
+          파티 이름:
+        </Text>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => setPartyName(text)}
+            value={partyName}
+          />
+        <Text>
+          인원 수:
+        </Text>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => setNumOfPeople(text)}
+            value={numOfPeople}
+            keyboardType={'numeric'}
+          />
+          <Text>
+          파티 소개:
+        </Text>
+          <TextInput
+            style={{ height: 150, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => setDescription(text)}
+            value={description}
+          />
+        
+      </View>
     </View>
   );
 };

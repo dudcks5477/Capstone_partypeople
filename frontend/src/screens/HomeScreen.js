b6f0be2cd0f1d1f31cb78fa930eeea33ecd82f2f
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-08 00:10:15 +0900

    const Stack = createNativeStackNavigator(); 안쓰여서 주석처리함

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import SearchBar from '../container/SearchBar';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Line from '../container/Line';
 import PartyCard from '../components/PartyCard'
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 const HomeScreen = () => (
   <>
     <SearchBar/>

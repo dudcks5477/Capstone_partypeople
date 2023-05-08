54453a72ef42c4baaf58f6b4026b5fdfcf7c5295
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-08 23:21:01 +0900

    데이터 삭제하는 기능하고, 날짜 한글 변환 시켜놓음

--- frontend/src/screens/AddScreen.js
+++ frontend/src/screens/AddScreen.js
@@ -2,11 +2,15 @@ import React, {useState} from 'react';
 import {View, Button, Platform, Text, TextInput,TouchableOpacity, Alert} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import CameraScreen from './CameraScreen';
-import PartyCard from '../components/PartyCard';
-import HomeScreen from './HomeScreen';
-const AddScreen = ({navigation, route}) => {
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { v4 as uuidv4 } from 'react-native-uuid';
+
+
+const AddScreen = ({navigation,route}) => {
   const { address,longitude,latitude } = route.params || {};
+
   const [date, setDate] = useState(new Date());
+  const [date2, setDate2] = useState(new Date());
   const [time, setTime] = useState();
   const [show, setShow] = useState(false);
   const [partyName, setPartyName] = useState('');
@@ -14,28 +18,41 @@ const AddScreen = ({navigation, route}) => {
   const [description, setDescription] = useState('');
   const [mode, setMode] = useState('date');
   
-  const handleCreate = () => {
+  const handleCreate = async() => {
+    
     if(!address || !partyName || !numOfPeople || !description || !date ) {
       Alert.alert('오류', '입력되지 않은 항목이 있습니다.');
     } else {
-      setTime(date.toLocaleTimeString())
-      navigation.navigate('Map', {
-        address : address,
-        longitude: longitude,
-        latitude: latitude,
-        partyName: partyName,
-        numOfPeople: numOfPeople,
-        description: description,
-        date: date.toLocaleDateString(),
-        time: date.toLocaleTimeString(),
-      });
+      
+      navigation.navigate('Map');
+      const data = { address, longitude, latitude, date2, time, partyName, numOfPeople, description };
+      try {
+        await AsyncStorage.setItem('partyData',JSON.stringify(data));
+        navigation.navigate('Map');
+      }catch(e){
+        console.log(e)
+      }
+    }
+  } 
+  const handleClearAsyncStorage = async () => {
+    try {
+      await AsyncStorage.removeItem('partyData');
+      Alert.alert('알림', '저장된 데이터가 삭제되었습니다.');
+    } catch(e) {
+      console.log(e);
     }
   }
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
+    const currentDate2 = selectedDate || date;
     setShow(Platform.OS === 'ios');
     setDate(currentDate);
+    setDate2(currentDate2.toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' }))
+    console.log('aaD',currentDate.toLocaleDateString());
+    console.log("AAd",currentDate.toLocaleTimeString())
+    setTime(currentDate.toLocaleTimeString());
+
   };
 
   const showMode = currentMode => {
@@ -50,11 +67,13 @@ const AddScreen = ({navigation, route}) => {
     <View>
       <View>
         <Button onPress={showDatepicker} title="Show date picker!" />
-        <Text>선택한 날짜: {date.toLocaleDateString()}</Text>
+        <Text>선택한 날짜: {date.toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' })}</Text>
+        
       </View>
       <View>
         <Button onPress={showTimepicker} title="Show time picker!" />
         <Text>선택한 시간: {mode === 'time' && date.toLocaleTimeString()}</Text>
+        
       </View>
       {show && (
         <DateTimePicker
@@ -95,14 +114,17 @@ const AddScreen = ({navigation, route}) => {
       <TouchableOpacity onPress={handleCreate}>
         <Text>생성하기</Text>
       </TouchableOpacity>
-      <HomeScreen
+      <TouchableOpacity onPress={handleClearAsyncStorage}>
+    <Text>저장된 데이터 삭제</Text>
+  </TouchableOpacity>
+      {/* <HomeScreen
         address={address}
         partyName={partyName}
         numOfPeople={numOfPeople}
         description={description}
         date={date.toLocaleDateString()}
         time= {date.toLocaleTimeString()}
-        />
+        /> */}
     </View>
   );
 };

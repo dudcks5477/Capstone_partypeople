759bfc4c09f3a21f2c8c3a48c8077e6e19d891e0
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-08 23:21:28 +0900

    addscreen 데이터 homescreen 에서 직접 받음

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // AsyncStorage import 추가
 import SearchBar from '../container/SearchBar';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Line from '../container/Line';
-import PartyCard from '../components/PartyCard'
-// const Stack = createNativeStackNavigator();
-const HomeScreen = ({ address, partyName, numOfPeople, description, date, time }) => (
-  <>
-    <SearchBar />
-    <Line style={{ marginTop: 20 }} />
-    <PartyCard
-    address={address}
-    partyName={partyName}
-    numOfPeople={numOfPeople}
-    description={description}
-    date={date}
-    time= {time} />
-  </>
-);
+import Card from '../components/Card';
 
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [partyData, setPartyData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const partyData = await AsyncStorage.getItem('partyData');
+        if (partyData !== null) {
+          setPartyData(JSON.parse(partyData));
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <>
+      <SearchBar />
+      <Line style={{ marginTop: 20 }} />
+      <SafeAreaView style={styles.containerNotch}>
+        <View style={styles.container}>
+          <ScrollView>
+          {partyData && (
+              <TouchableOpacity
+                style={styles.cardButton}
+                onPress={() => navigation.navigate('PartyDetail')}
+                activeOpacity={1}>
+                <Card partyData={partyData}/>
+              </TouchableOpacity>
+            )}
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerNotch: {
+    flex: 1,
+  },
+  container: {
+    marginVertical: 10,
+  },
+  cardButton: {
+    marginBottom: 10,
+  },
+});
 
 export default HomeScreen;

5b2a5aa3500e820897258873d65d09763c98d274
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-08 23:20:02 +0900

    partycard 없애고 card 만듬

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,59 +1,71 @@
-import React from 'react';
-import {View, Image, Text, StyleSheet, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import Config from 'react-native-config';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const Card = ({image, address, partyName, numOfPeople, description, date, time}) => {
-  console.log("CARd",image, address, partyName, numOfPeople, description, date, time);
+const Card = ({partyData}) => {
+  // const [partyData, setPartyData] = useState({
+  //   address: '',
+  //   date: '',
+  //   time: '',
+  //   partyName: '',
+  //   numOfPeople: '',
+  //   description: '',
+  // });
+
   return (
-    <View style={styles.cardContainer}>
-      <View style={styles.card}>
-        <Image source={image} style={styles.image} />
-        <Text style={styles.title}>{partyName}</Text>
-        <Text style={styles.description}>{description}</Text>
-        <Text style={styles.title}>{numOfPeople}</Text>
-        <Text style={styles.title}>{date}</Text>
-        <Text style={styles.title}>{time}</Text>
-        <Text style={styles.title}>{address}</Text>
-      </View>
-    </View>
-  );
+    <>
+      <View style={styles.cardContainer}>
+        {/* 여기다가 TouchableOpacity만들어서 보내보자 내생각엔 */}
+        <View style={styles.card}>
+          {/* <Image source={image} style={styles.image} /> */}
+          <Text style={styles.title}>{partyData.partyName}</Text>
+          <Text style={styles.description}>{partyData.description}</Text>
+          <Text style={styles.title}>{partyData.numOfPeople}</Text>
+          <Text style={styles.title}>{partyData.date}</Text>
+<Text style={styles.title}>{partyData.time}</Text>
+<Text style={styles.title}>{partyData.address}</Text>
+</View>
+</View>
+</>
+);
 };
-//홈으로가서 party card한테 변수 넘겨서 바꿔야될듯
+
 const styles = StyleSheet.create({
-  cardContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  card: {
-    backgroundColor: '#fff',
-    borderRadius: 16,
-    overflow: 'hidden',
-    elevation: 5,
-    margin: 10,
-    width: '90%',
-    aspectRatio: 1.5,
-  },
-  image: {
-    width: '100%',
-    height: '60%',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    marginHorizontal: 10,
-    marginTop: 10,
-  },
-  description: {
-    fontSize: 14,
-    marginHorizontal: 10,
-    marginVertical: 5,
-  },
-  price: {
-    fontSize: 16,
-    marginHorizontal: 10,
-    marginBottom: 10,
-    fontWeight: 'bold',
-  },
+cardContainer: {
+alignItems: 'center',
+justifyContent: 'center',
+},
+card: {
+backgroundColor: '#fff',
+borderRadius: 16,
+overflow: 'hidden',
+elevation: 5,
+margin: 10,
+width: '90%',
+aspectRatio: 1.5,
+},
+image: {
+width: '100%',
+height: '60%',
+},
+title: {
+fontWeight: 'bold',
+fontSize: 16,
+marginHorizontal: 10,
+marginTop: 10,
+},
+description: {
+fontSize: 14,
+marginHorizontal: 10,
+marginVertical: 5,
+},
+price: {
+fontSize: 16,
+marginHorizontal: 10,
+marginBottom: 10,
+fontWeight: 'bold',
+},
 });
 
 export default Card;

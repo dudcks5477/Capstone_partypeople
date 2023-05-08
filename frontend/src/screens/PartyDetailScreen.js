2e9b2bc14e56e4a9ad5d9324ce27a0afbab0c97c
dudcks5477 <dudcks5477@naver.com>
2023-05-08 23:14:51 +0900

    Add Event

--- frontend/src/screens/PartyDetailScreen.js
+++ frontend/src/screens/PartyDetailScreen.js
@@ -21,6 +21,13 @@ const PartyDetailScreen = () => {
     // 참가자 버튼을 눌렀을 때 동작
     console.log("참가자 버튼이 눌렸습니다.");
   }
+
+  const images = [
+    require('../assets/party1.jpeg'),
+    require('../assets/party2.jpeg'),
+    require('../assets/party3.jpeg'),
+    // 이미지 경로 추가
+  ]
   
   return (
     <ScrollView>
@@ -35,6 +42,7 @@ const PartyDetailScreen = () => {
         <MaterialIcons name="chevron-left" size={24} color="black" style={{ marginRight: 2}} />
         <Text>Chan's Party</Text>
       </TouchableOpacity>
+
       <View style={styles.cardContainer}>
         <Image source={require('../assets/party1.jpeg')} style={styles.cardImage} />
       </View>
@@ -52,7 +60,7 @@ const PartyDetailScreen = () => {
 
       <Line/>
 
-      <View style={{width:"90%", marginHorizontal:"5%", height:136}}>
+      <View style={{width:"90%", marginHorizontal:"5%", height:177}}>
         <Text>파티소개</Text>
       </View>
 
@@ -80,12 +88,13 @@ const PartyDetailScreen = () => {
       <View style={styles.buttonContainer}>
         <View style={styles.cartContainer}>
           <MaterialIcons name="shopping-cart" size={28} color="gray" />
-          <Text style={styles.cartText}>100</Text>
+          {/* <Text style={styles.cartText}>100</Text> */}
         </View>
-        <Button
-          title="참석하기"
-          color="gray"
-          onPress={() => Alert.alert("참석하기를 누르셨습니다!")} />
+          <Button
+            title="참석하기"
+            color="gray"
+            onPress={() => Alert.alert("참석하기를 누르셨습니다!")}
+          />
         <MaterialIcons name="favorite-border" size={28} color="gray" />
       </View>
     </ScrollView>
@@ -159,9 +168,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   buttonContainer: {
+    margin: 5,
     width: '90%',
     marginHorizontal: '5%',
-    marginTop: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',

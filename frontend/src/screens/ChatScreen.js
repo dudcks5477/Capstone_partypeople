e3bd171813b2f400d846a7acca47dedbfba9b416
dudcks5477 <dudcks5477@naver.com>
2023-05-14 00:40:37 +0900

    ChatScreen Design

--- frontend/src/screens/ChatScreen.js
+++ frontend/src/screens/ChatScreen.js
@@ -5,6 +5,12 @@ import Line from '../container/Line';
 
 const ChatScreen = ({ navigation }) => {
   const [chatRooms, setChatRooms] = useState([]);
+  // add this state
+  const [messageCount, setMessageCount] = useState(0);
+  // add this state
+  const [notificationCount, setNotificationCount] = useState(0);
+
+  const [selectedTab, setSelectedTab] = useState('message');
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
@@ -38,19 +44,75 @@ const ChatScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.title}>채팅</Text>
-      <FlatList
+      
+      {/* 일단 뭔지 몰라서 주석처리 */}
+      {/* <FlatList
         data={chatRooms}
         renderItem={renderItem}
         keyExtractor={item => item.partyName}
-      />
-      <Line />
+      /> */}
+      <View style={{
+        marginTop: 20, 
+        width: '90%',
+        marginHorizontal: '5%',
+        }}>
+         <View style={{flexDirection: 'row', marginBottom:13}}>
+          <TouchableOpacity onPress={() => setSelectedTab('message')}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text style={{marginRight: 35}}>Message</Text>
+              {messageCount > 0 && (
+                <View style={{
+                  backgroundColor: 'black',
+                  borderRadius: 50,
+                  width: 20,
+                  height: 20,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginLeft: 5,
+                }}>
+                  <Text style={{
+                    color: 'white',
+                    fontSize: 12,
+                  }}>{messageCount}</Text>
+                </View>
+              )}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setSelectedTab('notification')}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text>Notification</Text>
+              {notificationCount > 0 && (
+                <View style={{
+                  backgroundColor: 'black',
+                  borderRadius: 50,
+                  width: 20,
+                  height: 20,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginLeft: 5,
+                }}>
+                  <Text style={{
+                    color: 'white',
+                    fontSize: 12,
+                  }}>{notificationCount}</Text>
+                </View>
+              )}
+            </View>
+          </TouchableOpacity>
+        </View>
+        {selectedTab === 'message' && <Line style={{width: 70, backgroundColor: 'black'}}/>}
+        {selectedTab === 'notification' && <Line style={{width: 100, marginLeft: '50%', backgroundColor: 'black'}}/>}
+      </View>
+
+      <Line style= {{marginBottom: 10}}/>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   title: {
-    color: '#000',
+    color: 'gray',
     fontSize: 40,
     fontWeight: 'bold',
     marginLeft: 15,

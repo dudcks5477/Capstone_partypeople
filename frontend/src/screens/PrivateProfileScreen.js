b00f8ca1421ebd4e578e3938c8690b783bce2371
dudcks5477 <dudcks5477@naver.com>
2023-05-16 17:59:52 +0900

    Profile Detail

--- frontend/src/screens/PrivateProfileScreen.js
+++ frontend/src/screens/PrivateProfileScreen.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Line from '../container/Line';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const PrivateProfileScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Private Profile Screen</Text>
+    <View style={{
+      
+     }}>
+      <Text style={{
+        color: 'gray',
+        fontSize: 40,
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginTop: 30,
+      }}>프로필</Text>
+      <Line style={{
+        marginTop: 20, 
+        width: '90%',
+        marginHorizontal: '5%',
+      }} />
+      <View style={{
+        marginTop: 5,
+        width: '90%',
+        marginHorizontal: '5%',
+        flexDirection: 'row',
+      }}>
+        <MaterialIcons name="account-circle" size={60} color="gray" style={{marginRight: 2}}/>
+        <View style={{flexDirection: 'column', justifyContent: 'center'}}>
+          <Text style={{fontSize: 14}}>주최자</Text>
+          <Text style={{fontSize: 12}}>Show profile</Text>
+          <MaterialIcons name="chevron-right" size={20} color="gray"/>
+        </View>
+      </View>
     </View>
   );
 };

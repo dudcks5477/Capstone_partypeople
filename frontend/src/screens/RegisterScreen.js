977d278235a8f540c3ea2ce9e249b9b7ad561907
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-17 19:00:43 +0900

    회원 가입 페이지

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity ,Text} from 'react-native';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import { styles } from './Styles/LoginStyles';
+const RegisterScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [birthDay, setBirthDay] = useState('');
+  const navigation = useNavigation();
+  const registerUser = async () => {
+    try {
+      const response = await axios.post('http://your-backend-url.com/register', {
+        name: name,
+        email: email,
+        password: password,
+        birthDay: birthDay,
+      });
+      
+      // 회원가입이 성공적으로 처리되었을 때의 로직을 작성합니다.
+      console.log(response.data); // 서버로부터 받은 응답 데이터 출력
+      navigation.navigate('Login')
+    } catch (error) {
+      // 회원가입 요청이 실패하였을 때의 예외 처리 로직을 작성합니다.
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <TextInput
+        placeholder="이름"
+        value={name}
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        placeholder="이메일"
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        placeholder="비밀번호"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <TextInput
+        placeholder="생년월일"
+        value={birthDay}
+        onChangeText={text => setBirthDay(text)}
+      />
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={registerUser}>
+        <Text style={styles.buttonText}>회원가입</Text>
+      </TouchableOpacity>
+    </View>
+    </>
+  );
+};
+
+export default RegisterScreen;

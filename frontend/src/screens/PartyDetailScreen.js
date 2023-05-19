9145853644cdc404a06e6be595b44a38706265ce
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-20 00:42:46 +0900

    참석하기 누르면 채팅방 들어가지고 참석자 데이터를 백엔드로 넘기는 코드 구현, 위시리스트도 백엔드로 보냄 useEffect() 는 주석 처리 해놓음

--- frontend/src/screens/PartyDetailScreen.js
+++ frontend/src/screens/PartyDetailScreen.js
@@ -1,11 +1,10 @@
-import React ,{useState,useEffect} from 'react';
-import {TouchableOpacity, Text, StyleSheet, Image, View, ScrollView, Button, Alert} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Text, StyleSheet, Image, View, ScrollView, Button, Alert } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 import Line from '../container/Line';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { TouchableHighlight } from 'react-native-gesture-handler';
-import ChatRoomScreen from './ChatRoomScreen';
 const PartyDetailScreen = () => {
   const [isFavorite, setIsFavorite] = useState(false);
   const [partyData, setPartyData] = useState({
@@ -16,93 +15,58 @@ const PartyDetailScreen = () => {
     numOfPeople: '',
     description: '',
   });
-  
-  const toggleFavorite = async () => {
-    const newFavoriteValue = !isFavorite;
-    setIsFavorite(newFavoriteValue);
-  
+
+  const navigation = useNavigation();
+
+  const fetchPartyDetail = async () => {
     try {
-      const wishlist = await AsyncStorage.getItem('wishlist');
-      let currentWishlist = wishlist ? JSON.parse(wishlist) : [];
-  
-      if (newFavoriteValue) {
-        // 찜 목록에 추가
-        currentWishlist.push({
-          address: partyData.address,
-          date: partyData.date,
-          time: partyData.time,
-          partyName: partyData.partyName,
-          numOfPeople: partyData.numOfPeople,
-          description: partyData.description,
-        });
-      } else {
-        // 찜 목록에서 삭제
-        currentWishlist = currentWishlist.filter(
-          (item) => item.partyName !== partyData.partyName
-        );
-      }
-  
-      await AsyncStorage.setItem('wishlist', JSON.stringify(currentWishlist));
+      const response = await axios.get('http://your-backend-server-url/api/partyDetail', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      setPartyData(response.data);
     } catch (e) {
-      console.log(e);
+      console.error(e);
+    }
+  };
+
+  const toggleFavorite = async () => {
+    try {
+      const response = await axios.post('http://your-backend-server-url/api/toggleFavorite', {
+        partyName: partyData.partyName,
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      setIsFavorite(response.data.isFavorite);
+    } catch (e) {
+      console.error(e);
     }
   };
 
 
-  const navigation = useNavigation();
   
-const joinChatRoom = async () => {
-  try {
-    const chatRooms = await AsyncStorage.getItem('chatRooms');
-    let currentChatRooms = chatRooms ? JSON.parse(chatRooms) : [];
-
-    // 이미 해당 partyName의 채팅방이 있는지 확인
-    const chatRoomExists = currentChatRooms.some((chatRoom) => chatRoom.partyName === partyData.partyName);
-
-    if (!chatRoomExists) {
-      // 존재하지 않으면 새 채팅방 추가
-      currentChatRooms.push({
+  const joinChatRoom = async () => {
+    try {
+      await axios.post('http://your-backend-server-url/api/joinChatRoom', {
         partyName: partyData.partyName,
-      });
-
-      await AsyncStorage.setItem('chatRooms', JSON.stringify(currentChatRooms));
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await AsyncStorage.getItem('partyData');
-        if (data !== null) {
-          setPartyData(JSON.parse(data));
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
         }
-      } catch (e) {
-        console.log("Error fetching data", e);
-      }
-    };
-  
-    fetchData();
-  }, []);
-  
+      });
+      navigation.navigate('Chat');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   useEffect(() => {
-    const checkIfFavorite = async () => {
-      try {
-        const wishlist = await AsyncStorage.getItem('wishlist');
-        const currentWishlist = wishlist ? JSON.parse(wishlist) : [];
-        const isPartyInWishlist = currentWishlist.some(
-          (item) => item.partyName === partyData.partyName
-        );
-        setIsFavorite(isPartyInWishlist);
-      } catch (e) {
-        console.log('Error checking favorite status:', e);
-      }
-    };
-  
-    checkIfFavorite();
-  }, [partyData]);
+    // fetchPartyDetail();
+  }, []);
+
 
   const handleGoBack = () => {
     navigation.goBack(); // 이전으로 돌아가기
@@ -142,16 +106,17 @@ const joinChatRoom = async () => {
         <Image source={require('../assets/party1.jpeg')} style={styles.cardImage} />
       </View>
 
-      <TouchableOpacity onPress={() => navigation.navigate('ProfileScreen')} style={styles.hostProfile}>
-        <View style={styles.profileTextContainer}>
-          <MaterialIcons name="account-circle" size={50} color="gray" />
-          <View>
-            <Text>김영찬</Text>
-            <Text>Show profile</Text>
-          </View>
-        </View>
-        <MaterialIcons name="chevron-right" size={24} color="black" style={{marginRight:8}}/>
-      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('ProfileScreen', { userId: partyData.hostId })} style={styles.hostProfile}>
+  <View style={styles.profileTextContainer}>
+    <MaterialIcons name="account-circle" size={50} color="gray" />
+    <View>
+      <Text>{partyData.hostName}</Text>
+      <Text>Show profile</Text>
+    </View>
+  </View>
+  <MaterialIcons name="chevron-right" size={24} color="black" style={{marginRight:8}}/>
+</TouchableOpacity>
+
 
       <Line/>
 
@@ -188,28 +153,18 @@ const joinChatRoom = async () => {
       
           {/* <Text style={styles.cartText}>100</Text> */}
         </View>
-        <Button
-  title="참석하기"
-  color="gray"
-  onPress={async () => {
-    await joinChatRoom();
-    navigation.navigate('Chat');
-  }}
-/>
+        <Button title="참석하기" color="gray" onPress={joinChatRoom}/>
     
       <View>
-      <TouchableOpacity onPress={toggleFavorite} >
-        <View>
-          {isFavorite ? (
-            <MaterialIcons name="favorite" size={28} color="red" />
-          ) : (
-            <MaterialIcons name="favorite-border" size={28} color="gray" />
-          )}
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handlePress}>
-        
-      </TouchableOpacity>
+      <TouchableOpacity onPress={toggleFavorite}>
+      <View>
+        {isFavorite ? (
+          <MaterialIcons name="favorite" size={28} color="red" />
+        ) : (
+          <MaterialIcons name="favorite-border" size={28} color="gray" />
+        )}
+      </View>
+    </TouchableOpacity>
       </View>
     </ScrollView>
   );

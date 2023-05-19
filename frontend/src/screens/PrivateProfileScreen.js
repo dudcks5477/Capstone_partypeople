43faf889fb5531bfa29161ce94dc315ea6c26bb0
dudcks5477 <dudcks5477@naver.com>
2023-05-19 14:37:17 +0900

    except Event Handler

--- frontend/src/screens/PrivateProfileScreen.js
+++ frontend/src/screens/PrivateProfileScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Line from '../container/Line';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const PrivateProfileScreen = () => {
+  
+  const onItemPress = (itemName) => {
+    console.log(`${itemName} clicked`);
+    
+  }
+
   return (
     <View style={{
       
@@ -30,8 +36,39 @@ const PrivateProfileScreen = () => {
         <View style={{flexDirection: 'column', justifyContent: 'center'}}>
           <Text style={{fontSize: 14}}>주최자</Text>
           <Text style={{fontSize: 12}}>Show profile</Text>
-          <MaterialIcons name="chevron-right" size={20} color="gray"/>
         </View>
+      </View>
+      <View style={{
+        width: '90%', 
+        marginHorizontal: "5%",
+      }}>
+        <Text style={{
+          marginTop: 5,
+          fontSize: 20,
+          fontWeight: 'bold'}}>참석 예정</Text>
+          {/* 여기에 파티 요약본 넣어야함 */}
+      </View>
+      <Line/>
+      <View style={{
+        width: '90%', 
+        marginHorizontal: "5%",
+      }}>
+        <Text style={{
+          marginTop: 5,
+          fontSize: 20,
+          fontWeight: 'bold'}}>세팅</Text>
+        <TouchableOpacity onPress={() => onItemPress('Personal Information')}>
+          <Text>Personal Information</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => onItemPress('Login & Security')}>
+          <Text>Login & Security</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => onItemPress('Payments and payouts')}>
+          <Text>Payments and payouts</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => onItemPress('Taxes')}>
+          <Text>Taxes</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );

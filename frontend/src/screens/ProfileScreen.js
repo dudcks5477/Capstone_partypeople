381090e27b8cd4235e4fd4ac64cbd3d7fc7cd9af
HyukJunJi <whrnjsrkdls@gmail.com>
2023-05-20 00:43:19 +0900

    백엔드랑 데이터 주고 받을 수 있게 코드 수정

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,111 +1,162 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Button, FlatList } from 'react-native';
 import { Rating } from 'react-native-ratings'; 
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Line from '../container/Line';
+import axios from 'axios';
+import PartyItem from './PartyItem';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const [hostedParties, setHostedParties] = useState([]);
+  const [participatedParties, setParticipatedParties] = useState([]);
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [userDetails, setUserDetails] = useState({ subscribers: 0, idCertified: false, cellPhoneCertified: false, rating: 0 });
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Assuming these are the correct URLs
+    axios.get('/api/hostedParties')
+      .then(response => setHostedParties(response.data))
+      .catch(error => console.log(error));
+
+    axios.get('/api/participatedParties')
+      .then(response => setParticipatedParties(response.data))
+      .catch(error => console.log(error));
+
+    axios.get('/api/userDetails')
+      .then(response => setUserDetails(response.data))
+      .catch(error => console.log(error));
+  }, []);
 
   const handleGoBack = () => {
-    navigation.goBack(); // 이전으로 돌아가기
+    navigation.goBack(); 
   }
+
+  const handleSubscribe = () => {
+    // Assuming this is the correct API call to subscribe
+    axios.post('/api/subscribe')
+      .then(() => setIsSubscribed(true))
+      .catch(error => console.log(error));
+  }
+
+  const handlePartySelect = (partyId) => {
+    // Assuming 'PartyDetail' is the name of your party detail screen
+    navigation.navigate('PartyDetail', { partyId });
+  }
+
+  const renderPartyItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handlePartySelect(item.id)}>
+      <PartyItem party={item} />
+    </TouchableOpacity>
+  );
 
   return (
     <View>
-      {/* onPress부분 누르지 말기 에러뜸 */}
-      <TouchableOpacity onPress={handleGoBack} style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: '90%',
-        marginHorizontal: '5%',
-        marginTop: 20,
-        marginBottom: 10}}>
+      <TouchableOpacity onPress={handleGoBack} style={styles.topNav}>
         <MaterialIcons name="chevron-left" size={40} color="gray" style={{ marginRight: 2}} />
-        <Text style={{
-          color: 'gray',
-          fontSize: 40,
-          fontWeight: 'bold',
-        }}>프로필</Text>
+        <Text style={styles.profileText}>프로필</Text>
       </TouchableOpacity>
-
+      
       <Line/>
-      <View style={{
-        width: '90%',
-        marginHorizontal: '5%',
-        marginTop: 10,
-        flexDirection: 'row'
-      }}>
+      <View style={styles.userInfo}>
         <MaterialIcons name="account-circle" size={60} color="gray" style={{marginRight: 2}}/>
         <View style={{
             marginLeft: 50,
             alignItems: 'center',
           }}>
           <Text >Subscribers</Text>
-          {/* 구독자 수 기능 */}
           <Text style={{
             marginTop: 5
-          }}>10</Text>
+          }}>{userDetails.subscribers}</Text>
         </View>
-        
-        {/* 인증 마크 왕관 못찾는 중 */}
-        <View style={{flex: 1, alignItems: 'flex-end'}}>
-          <View style={{
-            borderWidth: 1,
-            borderColor: '#e1d45a',
-            borderRadius: 5,
-            flexDirection: 'row',
-            padding: 2,
-            height: 25,
-            marginLeft: 10,
-            backgroundColor: '#e1d45a'
-          }}>
+        {userDetails.idCertified && 
+          <View style={styles.certifiedContainer}>
             <Text style={{ color: 'black', marginLeft: 5}}>ID certified</Text>
           </View>
-          <View style={{
-            borderWidth: 1,
-            borderColor: '#e1d45a',
-            borderRadius: 5,
-            flexDirection: 'row',
-            padding: 2,
-            height: 25,
-            marginLeft: 10,
-            marginTop: 4,
-            backgroundColor: '#e1d45a'
-          }}>
-            <Text style={{ color: 'black', marginLeft: 5}}>cellphone certified</Text>
+        }
+        {userDetails.cellPhoneCertified && 
+          <View style={styles.certifiedContainer}>
+            <Text style={{ color: 'black', marginLeft: 5}}>Cellphone certified</Text>
           </View>
-        </View>
-
+        }
       </View>
-      <View style={{ alignSelf: 'flex-start', marginLeft: '5%', marginTop: 10}}>
-        {/* Rating 패키지 다운로드 해야함 */}
+      <View style={styles.ratingContainer}>
         <Rating
-        imageSize={20}
-        readonly
-        startingValue={5}
-        style={{ paddingVertical: 10,}}
+          imageSize={20}
+          readonly
+          startingValue={userDetails.rating}
+          style={{ paddingVertical: 10,}}
         />
       </View>
-      <View style={{
-        width: '90%',
-        marginHorizontal: '5%'
-      }}>
+      <Button 
+        title={isSubscribed ? '구독중' : '구독하기'}
+        onPress={handleSubscribe}
+        disabled={isSubscribed}
+      />
+      <View style={styles.section}>
         <Text>Recent Party / Host</Text>
-        {/* 이미지 */}
-        <Text>Comment</Text>
+        <FlatList 
+          data={hostedParties}
+          renderItem={renderPartyItem}
+          keyExtractor={item => item.id}
+        />
+        {/* Add Comments Here */}
       </View>
       <Line style={{marginTop: 40}}/>
-      <View style={{
-        width: '90%',
-        marginHorizontal: '5%',
-        marginTop: 5
-      }}>
+      <View style={styles.section}>
         <Text>Recent Party / Participate In</Text>
-        {/* 이미지 */}
-        <Text>Comment</Text>
+        <FlatList 
+          data={participatedParties}
+          renderItem={renderPartyItem}
+          keyExtractor={item => item.id}
+        />
+        {/* Add Comments Here */}
       </View>
     </View>
   );
 };
+
+const styles = {
+  topNav: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '90%',
+    marginHorizontal: '5%',
+    marginTop: 20,
+    marginBottom: 10
+  },
+  profileText: {
+    color: 'gray',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  userInfo: {
+    width: '90%',
+    marginHorizontal: '5%',
+    marginTop: 10,
+    flexDirection: 'row'
+  },
+  certifiedContainer: {
+    borderWidth: 1,
+    borderColor: '#e1d45a',
+    borderRadius: 5,
+    flexDirection: 'row',
+    padding: 2,
+    height: 25,
+    marginLeft: 10,
+    backgroundColor: '#e1d45a'
+  },
+  ratingContainer: {
+    alignSelf: 'flex-start', 
+    marginLeft: '5%', 
+    marginTop: 10
+  },
+  section: {
+    width: '90%',
+    marginHorizontal: '5%',
+  }
+}
 
 export default ProfileScreen;

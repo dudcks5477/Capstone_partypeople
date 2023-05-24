6cad3a0044c82674fcc1dff9c3a95d05ae61ec0c
dudcks5477 <dudcks5477@naver.com>
2023-05-24 16:45:06 +0900

    chat bar modify but is not complete

--- frontend/src/screens/ChatRoomScreen.js
+++ frontend/src/screens/ChatRoomScreen.js
@@ -1,32 +1,45 @@
 import React, { useState } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, InputToolbar, Composer, Send } from 'react-native-gifted-chat';
+import { View, Text } from 'react-native';
 
+const CustomToolbar = (props) => {
+  return (
+      <InputToolbar {...props} containerStyle={{ backgroundColor: 'black', justifyContent: 'flex-end' }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Composer {...props} textInputStyle={{ color: 'white' }} />
+              <Send {...props} alwaysShowSend>
+                  <View style={{ marginRight: 10, marginBottom: 0 }}>
+                      <Text style={{ color: 'white' }}>Send</Text>
+                  </View>
+              </Send>
+          </View>
+      </InputToolbar>
+  );
+};
 
 const ChatRoomScreen = ({ route }) => {
-  const { partyName } = route.params;
-  const [messages, setMessages] = useState([]);
+    const { partyName } = route.params;
+    const [messages, setMessages] = useState([]);
 
-  function onSend(newMessages = []) {
-    setMessages(GiftedChat.append(messages, newMessages));
-  }
+    function onSend(newMessages = []) {
+        setMessages(GiftedChat.append(messages, newMessages));
+    }
 
-  return (
-      <GiftedChat
-        messages={messages}
-        onSend={newMessages => onSend(newMessages)}
-        user={{
-          _id: 1,
-          name: partyName,
-        }}
-        placeholder='메시지를 입력하세요.'
-        placeholderTextColor="#fff"
-        textInputStyle={{
-          backgroundColor: "#000",
-          color: "#fff",
-          marginLeft: 10
-        }}
-      />
-  );
+    return (
+        <GiftedChat
+            messages={messages}
+            onSend={newMessages => onSend(newMessages)}
+            user={{
+                _id: 1,
+                name: partyName,
+            }}
+            renderInputToolbar={(props) => <CustomToolbar {...props} />}
+            textInputProps={{
+              placeholder: "Write a message",
+              color: 'white'
+            }}
+        />
+    );
 };
 
 export default ChatRoomScreen;
